8d97f2c5af287f46094fcdc1555468d93e08a14b
devtoju <msg.toju@outlook.de>
2021-09-16 07:19:39 +0000

    interest calculator changes

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
+function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
-        
+
+function compute()
+{   
+    var principal = document.getElementById("principal");
+	if(principal.value <= 0)
+	{
+		alert("Enter a positiv number");
+		principal.focus();
+		return;
+	}
+	
+	principal = principal.value;
+	
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+    var amount = principal * years * rate / 100;
+	
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+
+		"</mark>,\<br\>at an interest rate of <mark>"+rate+
+		"</mark>%\<br\>You will receive an amount of <mark>"+amount+
+		"</mark>,\<br\>in the year <mark>"+year+"\<br\>";
+}
